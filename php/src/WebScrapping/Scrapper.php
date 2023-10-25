d4e3b52280cac5d01e03643706b0a1ec6106b356
Gustavo Viana <gustaolive1234@gmail.com>
2023-10-25 12:45:10 -0300

    Testando phplint

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -29,7 +29,8 @@ class Scrapper {
     $headerRow = WriterEntityFactory::createRowFromArray(['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution',
       'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution',
       'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution',
-      'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution', ], $style);
+      'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution',
+    ], $style);
     $writer->addRow($headerRow);
     $cells = [];
     try {

4369737012b28bc7e98fa04fcc00167fdfed4b87
Gustavo Viana <gustaolive1234@gmail.com>
2023-11-09 22:14:01 -0200

    Testando IDtest

--- php/tests/Unit/WebScrapping/Entity/PaperTest.php
+++ php/tests/Unit/WebScrapping/Entity/PaperTest.php
@@ -15,11 +15,11 @@ class PaperTest extends TestCase {
    * Tests construct().
    */
   public function testConstruct() {
-    $paper = new Paper(123, 'Paper title', 'Oral presentation', [
+    $paper = new Paper(132, 'Paper title', 'Oral presentation', [
       new Person('Evariste Galois', 'Lycée Louis-le-Grand'),
     ]);
 
-    $this->assertEquals('123', $paper->id);
+    $this->assertEquals(132, $paper->id);
     $this->assertEquals('Paper title', $paper->title);
     $this->assertEquals('Oral presentation', $paper->type);
     $this->assertEquals('Evariste Galois', $paper->authors[0]->name);
